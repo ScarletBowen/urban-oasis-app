61c8bfc3103bc7e43fddbeb574410a11f1765eaf
Kaden Inskeep <tsukadenskeep@gmail.com>
2023-06-01 20:57:41 -0700

    server.js file

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
