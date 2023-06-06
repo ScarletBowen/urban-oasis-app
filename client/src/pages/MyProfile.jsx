a3c372855eb1f7d0afcd7c2f79bdd1809ca355d3
Danny Nguyen <dannynvn@gmail.com>
2023-06-05 21:25:58 -0700

    added basic code for rendering profile cards on profile pages

--- client/src/pages/MyProfile.jsx
+++ client/src/pages/MyProfile.jsx
@@ -1,6 +1,7 @@
 // import dependencies
 import React, { useState, useEffect } from 'react';
 import { useQuery, useMutation } from '@apollo/client';
+import { Navigate } from 'react-router-dom';
 
 // import queries and mutations
 import { GET_ME } from '../utils/queries';
@@ -12,13 +13,13 @@ import Nav from '../components/Nav';
 import MyProfileCard from '../components/MyProfileCard';
 
 
-export default function myProfile() {
+const myProfile = () => {
     // get logged-in user data
     // const { loading, data } = useQuery(GET_ME);
     // const user = data?.currentUser || {};
 
     return (
-        <MyProfileCard />
+        <Navigate to="myprofile" />
     );
 }
 
--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -1,1 +1,23 @@
+// import dependencies
 import React, { useState, useEffect } from 'react';
+import { useQuery, useMutation } from '@apollo/client';
+
+// import queries and mutations
+import { GET_USER } from '../utils/queries';
+
+
+// import components
+import Nav from '../components/Nav';
+// import Footer from '../components/Footer'; 
+import UserProfileCard from '../components/UserProfileCard';
+
+
+export default function userProfile() {
+    // get logged-in user data
+    // const { loading, data } = useQuery(GET_ME);
+    // const user = data?.currentUser || {};
+
+    return (
+        <UserProfileCard />
+    );
+}
