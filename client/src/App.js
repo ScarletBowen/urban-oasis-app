28eb77a9a4096e1704da3add3d224ea6ffedf5cb
Scarlet234 <109751916+Scarlet234@users.noreply.github.com>
2023-06-06 21:45:20 -0600

    adding map files

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 import * as parkData from "./data/parks.json";
 import "./app.css";
 
