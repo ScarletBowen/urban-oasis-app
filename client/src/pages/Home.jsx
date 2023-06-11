00a110a311837caa4f5447a74b59b6b979e3f79f
Scarlet234 <109751916+Scarlet234@users.noreply.github.com>
2023-06-11 15:16:08 -0600

    added footer

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { useState } from "react";
-import Nav from "../components/Nav";
 import Mapbox from "../components/Mapbox";
+import BackgroundImage from "../../assets/greenbg.jpg";
 
 const Home = () => {
   return (
-    <div>
+    <div style={{
+      backgroundImage: `url(${BackgroundImage})`,
+      backgroundPosition: 'center', 
+      backgroundSize: 'cover', 
+      backgroundRepeat: 'no-repeat'
+    }}>
+      <Nav /> {/* Inserted Nav component */}
       <h1>Irvine, California</h1>
       <Mapbox />
     </div>
