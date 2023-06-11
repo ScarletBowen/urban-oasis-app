15979aab703f5efe1a13429fca82bcabb1f5abd9
Danny Nguyen <dannynvn@gmail.com>
2023-06-11 13:48:24 -0700

    friendslist now says you have no friends

--- client/src/components/FriendsList.jsx
+++ client/src/components/FriendsList.jsx
@@ -6,23 +6,32 @@ const FriendsList = ({ friends }) => {
         <>
             <div className="mt-16 w-full p-3 flex flex-col items-center">
                 <h1>Friends List</h1>
-                {friends.map(friend => (
-                    <ul className="w-auto py-3 px-4 transition-all ease-in duration-300 hover:opacity-80">
-                        <li key={friend._id}>
-                            <div className="flex flex-row items-center text-sm">
-                                <img
-                                    src={friend.avatar}
-                                    alt={friend.username}
-                                    className="w-12 h-12 rounded-full"
-                                />
-                                <div className="ml-2 w-full flex flex-col">
-                                    <Link to={`/profile/${friend.username}`} className="font-medium hover:text-teal-400">{friend.username}</Link>
-                                    <p className="text-gray-400 text-xs">{friend.description}</p>
+                {friends.length === 0 ? (
+                    <p className="text-center">No friends yet 😔</p>
+                ) : (
+                    friends.map(friend => (
+                        <ul className="w-auto py-3 px-4 transition-all ease-in duration-300 hover:opacity-80">
+                            <li key={friend._id}>
+                                <div className="flex flex-row items-center text-sm">
+                                    <img
+                                        src={friend.avatar}
+                                        alt={friend.username}
+                                        className="w-12 h-12 rounded-full"
+                                    />
+                                    <div className="ml-2 w-full flex flex-col">
+                                        <Link to={`/profile/${friend.username}`} className="font-medium hover:text-teal-400">{friend.username}</Link>
+                                        <p className="text-gray-400 text-xs">{friend.description}</p>
+                                    </div>
                                 </div>
-                            </div>
-                        </li>
-                    </ul>
-                ))}
+                            </li>
+                        </ul>
+                    ))
+                )}
+               
+                    
+                
+                    
+    
             </div>
         </>
     );
