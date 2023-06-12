2ed49c5b5f4c4d571819939a5e8bb36fbdc6f1e3
Danny Nguyen <dannynvn@gmail.com>
2023-06-11 17:11:39 -0700

    quick edit to remove fullname

--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -91,10 +91,6 @@ const UserProfile = () => {
                 <div className="px-4 py-2">{user.username}</div>
               </div>
 
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold">Full Name</div>
-                <div className="px-4 py-2">{user.fullname}</div>
-              </div>
 
               <div className="grid grid-cols-2">
                 <div className="px-4 py-2 font-semibold">Bio</div>
